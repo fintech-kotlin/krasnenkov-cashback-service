cdad69934228da8d27046e8912b6e9f35c39dbe0
RKrasnenkov <RKrasnenkov@croc.ru>
2020-02-25 13:43:25 +0300

    Реализация двух методов в ДЗ№1

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/NotificationMessageGeneratorImpl.kt
@@ -6,7 +6,18 @@ class NotificationMessageGeneratorImpl(
     private val cardNumberMasker: CardNumberMasker
 ) : NotificationMessageGenerator {
 
+    /**
+     * Уважаемый, Сергей!
+     * Спешим Вам сообщить, что на карту ############4567
+     * начислен cashback в размере 700.00
+     * за категорию BLACK.
+     * Спасибо за покупку 2020-11-20T01:00
+     */
     override fun generateMessage(notificationMessageInfo: NotificationMessageInfo): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return "Уважаемый, ${notificationMessageInfo.name}!\n" +
+                "Спешим Вам сообщить, что на карту ${cardNumberMasker.mask(notificationMessageInfo.cardNumber)}\n" +
+                "начислен cashback в размере ${notificationMessageInfo.cashback}\n" +
+                "за категорию ${notificationMessageInfo.category}.\n" +
+                "Спасибо за покупку ${notificationMessageInfo.transactionDate}"
     }
 }
